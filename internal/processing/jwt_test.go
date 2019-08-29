5b6fade838f83a0bb086f377102e71f878577f96
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-08-29 09:10:22 +0200

    Add tests

--- internal/processing/jwt_test.go
+++ internal/processing/jwt_test.go
@@ -71,3 +71,49 @@ func TestGenerateAuthenticationPolicy(t *testing.T) {
 	assert.Equal(ap.Spec.Origins[0].Jwt.Issuer, "http://dex.someDomain.local")
 	assert.Equal(ap.Spec.Origins[0].Jwt.JwksURI, "http://dex-service.namespace.svc.cluster.local:5556/keys")
 }
+
+func TestOauthGenerateVirtualService(t *testing.T) {
+	assert := assert.New(t)
+
+	jwtStrategy := &jwt{JWKSURI: "http://dex-service.namespace.svc.cluster.local:5556/keys"}
+	vs := jwtStrategy.generateVirtualService(getGate())
+
+	assert.Equal(len(vs.Spec.Gateways), 1)
+	assert.Equal(vs.Spec.Gateways[0], apiGateway)
+
+	assert.Equal(len(vs.Spec.Hosts), 1)
+	assert.Equal(vs.Spec.Hosts[0], serviceHost)
+
+	assert.Equal(len(vs.Spec.HTTP), 1)
+	assert.Equal(len(vs.Spec.HTTP[0].Route), 1)
+	assert.Equal(len(vs.Spec.HTTP[0].Match), 1)
+	assert.Equal(vs.Spec.HTTP[0].Route[0].Destination.Host, "test-service.test-namespace.svc.cluster.local")
+	assert.Equal(int(vs.Spec.HTTP[0].Route[0].Destination.Port.Number), 8080)
+	assert.Equal(vs.Spec.HTTP[0].Match[0].URI.Regex, "/.*")
+
+	assert.Equal(vs.ObjectMeta.Name, "test-gate-test-service")
+	assert.Equal(vs.ObjectMeta.Namespace, "test-namespace")
+
+	assert.Equal(vs.ObjectMeta.OwnerReferences[0].APIVersion, "gateway.kyma-project.io/v2alpha1")
+	assert.Equal(vs.ObjectMeta.OwnerReferences[0].Kind, "Gate")
+	assert.Equal(vs.ObjectMeta.OwnerReferences[0].Name, "test-gate")
+	assert.Equal(vs.ObjectMeta.OwnerReferences[0].UID, types.UID("eab0f1c8-c417-11e9-bf11-4ac644044351"))
+
+}
+
+func TestPrepareAuthenticationPolicy(t *testing.T) {
+	assert := assert.New(t)
+
+	jwtStrategy := &jwt{JWKSURI: "http://dex-service.namespace.svc.cluster.local:5556/keys"}
+	gate := getGate()
+	jwtConfig := getJWTConfig()
+	jwtConfig.Issuer = "http://someIssuer.someDomain.com"
+
+	currentAP := jwtStrategy.generateAuthenticationPolicy(gate, getJWTConfig())
+	currentAP.ObjectMeta.Generation = int64(42)
+
+	newAP := jwtStrategy.prepareAuthenticationPolicy(gate, jwtConfig, currentAP)
+
+	assert.Equal(newAP.ObjectMeta.Generation, int64(42))
+	assert.Equal(newAP.Spec.Origins[0].Jwt.Issuer, jwtConfig.Issuer)
+}
