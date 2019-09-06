3f1f6414e8e538e9a89c1ec7064eed3f6e7f80ef
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2019-09-06 11:36:42 +0200

    Add builder prototype

---
+++ internal/builders/virtual_service.go
@@ -0,0 +1,225 @@
+package builders
+
+import (
+	k8sMeta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sTypes "k8s.io/apimachinery/pkg/types"
+	networkingv1alpha1 "knative.dev/pkg/apis/istio/common/v1alpha1"
+	networkingv1alpha3 "knative.dev/pkg/apis/istio/v1alpha3"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+//VirtualService
+
+type virtualService struct {
+	name      string
+	namespace string
+	owner     *ownerReference
+	spec      *virtualServiceSpec
+}
+
+func VirtualService(name string) *virtualService {
+	return &virtualService{
+		name: name,
+	}
+}
+
+func (b *virtualService) Namespace(ns string) *virtualService {
+	b.namespace = ns
+	return b
+}
+
+func (b *virtualService) Owner(o *ownerReference) *virtualService {
+	b.owner = o
+	return b
+}
+
+func (b *virtualService) Spec(s *virtualServiceSpec) *virtualService {
+	b.spec = s
+	return b
+}
+
+func (b *virtualService) Get() *networkingv1alpha3.VirtualService {
+	objectMeta := k8sMeta.ObjectMeta{
+		Name:      b.name,
+		Namespace: b.namespace,
+	}
+
+	if b.owner != nil {
+		objectMeta.OwnerReferences = []k8sMeta.OwnerReference{*b.owner.Get()}
+	}
+
+	vs := &networkingv1alpha3.VirtualService{
+		ObjectMeta: objectMeta,
+		Spec:       *b.spec.Get(),
+	}
+
+	return vs
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// OwnerReference
+
+type ownerReference struct {
+	val *k8sMeta.OwnerReference
+}
+
+func OwnerReference(name, apiVersion, kind, uid string) *ownerReference {
+	return &ownerReference{
+		val: &k8sMeta.OwnerReference{
+			Name:       name,
+			APIVersion: apiVersion,
+			Kind:       kind,
+			UID:        k8sTypes.UID(uid),
+		},
+	}
+}
+
+func (b *ownerReference) Controller(ctrl bool) *ownerReference {
+	b.val.Controller = &ctrl
+	return b
+}
+
+func (b *ownerReference) Get() *k8sMeta.OwnerReference {
+	return b.val
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// VirtualService.Spec
+
+type virtualServiceSpec struct {
+	hosts     []string
+	gateways  []string
+	matchReq  *matchRequest
+	routeDest *routeDestination
+}
+
+func VirtualServiceSpec() *virtualServiceSpec {
+	return &virtualServiceSpec{}
+}
+
+func (b *virtualServiceSpec) Host(host string) *virtualServiceSpec {
+	b.hosts = []string{host}
+	return b
+}
+
+func (b *virtualServiceSpec) Gateway(gw string) *virtualServiceSpec {
+	b.gateways = append(b.gateways, gw)
+	return b
+}
+
+func (b *virtualServiceSpec) HTTP(mr *matchRequest, rd *routeDestination) *virtualServiceSpec {
+	b.matchReq = mr
+	b.routeDest = rd
+	return b
+}
+
+func (b *virtualServiceSpec) Get() *networkingv1alpha3.VirtualServiceSpec {
+
+	var httpMatch []networkingv1alpha3.HTTPMatchRequest = nil
+	var routeDest []networkingv1alpha3.HTTPRouteDestination = nil
+
+	//var matchReq &networkingv1alpha3.HTTPMatchRequest = nil
+	if b.matchReq != nil {
+		httpMatch = append(httpMatch, *b.matchReq.data)
+		//matchReq = b.matchReq.data
+	}
+
+	//URI: &v1alpha1.StringMatch{
+	//	Regex: "/.*",
+	//},
+	//}
+
+	if b.routeDest != nil {
+		routeDest = append(routeDest, *b.routeDest.data)
+	}
+
+	//route := &networkingv1alpha3.HTTPRouteDestination{
+	//	Destination: networkingv1alpha3.Destination{
+	//		//Host: fmt.Sprintf("%s.%s.svc.cluster.local", *api.Spec.Service.Name, api.ObjectMeta.Namespace),
+	//		//Port: networkingv1alpha3.PortSelector{
+	//		//	Number: uint32(*api.Spec.Service.Port),
+	//		//},
+	//	},
+	//}
+
+	spec := &networkingv1alpha3.VirtualServiceSpec{
+		Hosts:    b.hosts,
+		Gateways: b.gateways,
+		HTTP: []networkingv1alpha3.HTTPRoute{
+			{
+				Match: httpMatch,
+				Route: routeDest,
+			},
+		},
+	}
+
+	return spec
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// MatchRequest
+
+func MatchRequest() *matchRequest {
+	return &matchRequest{}
+}
+
+type matchRequest struct {
+	data *networkingv1alpha3.HTTPMatchRequest
+}
+
+func (mr *matchRequest) URI() *stringMatch {
+	mr.data = &networkingv1alpha3.HTTPMatchRequest{}
+	mr.data.URI = &networkingv1alpha1.StringMatch{}
+	return &stringMatch{mr.data.URI, func() *matchRequest { return mr }}
+}
+
+type stringMatch struct {
+	data   *networkingv1alpha1.StringMatch
+	parent func() *matchRequest
+}
+
+func (st *stringMatch) Regex(value string) *matchRequest {
+	st.data.Regex = value
+	return st.parent()
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// routeDestination
+
+func RouteDestination() *routeDestination {
+	return &routeDestination{&networkingv1alpha3.HTTPRouteDestination{}}
+}
+
+type routeDestination struct {
+	data *networkingv1alpha3.HTTPRouteDestination
+}
+
+func (rd *routeDestination) Host(value string) *routeDestination {
+	rd.data.Destination.Host = value
+	return rd
+}
+func (rd *routeDestination) Port(value uint32) *routeDestination {
+	rd.data.Destination.Port.Number = value
+	return rd
+}
+
+func sliceIfDefined(e string) []string {
+	if e == "" {
+		return nil
+	}
+
+	return []string{e}
+}
+
+/*
+	spec := &networkingv1alpha3.VirtualServiceSpec{
+		Hosts:    []string{*api.Spec.Service.Host},
+		Gateways: []string{*api.Spec.Gateway},
+		HTTP: []networkingv1alpha3.HTTPRoute{
+			{
+				Match: []networkingv1alpha3.HTTPMatchRequest{*match},
+				Route: []networkingv1alpha3.HTTPRouteDestination{*route},
+			},
+		},
+	}
+*/
---
+++ internal/builders/virtual_service_test.go
@@ -0,0 +1,77 @@
+package builders
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Builder for", func() {
+
+	host := "oauthkeeper.cluster.local"
+	gateway := "some-gateway"
+
+	matchUriRegex := "*/"
+	destHost := "somehost.somenamespace.svc.cluster.local"
+	var destPort uint32 = 4321
+
+	Describe("VirtualService", func() {
+		It("should build the object", func() {
+			name := "testName"
+			namespace := "testNs"
+
+			refName := "refName"
+			refVersion := "v2alpha1"
+			refKind := "Gate"
+			refUID := "123"
+
+			vs := VirtualService(name).Namespace(namespace).
+				Owner(OwnerReference(refName, refVersion, refKind, refUID).Controller(true)).
+				Spec(
+					VirtualServiceSpec().
+						Host(host).
+						Gateway(gateway)).
+				Get()
+			fmt.Printf("%#v", vs)
+			Expect(vs.Name).To(Equal(name))
+			Expect(vs.Namespace).To(Equal(namespace))
+			Expect(vs.OwnerReferences).To(HaveLen(1))
+			Expect(vs.OwnerReferences[0].Name).To(Equal(refName))
+			Expect(vs.OwnerReferences[0].APIVersion).To(Equal(refVersion))
+			Expect(vs.OwnerReferences[0].Kind).To(Equal(refKind))
+			Expect(vs.OwnerReferences[0].UID).To(BeEquivalentTo(refUID))
+			Expect(vs.Spec.Hosts).To(HaveLen(1))
+			Expect(vs.Spec.Hosts[0]).To(Equal(host))
+			Expect(vs.Spec.Gateways).To(HaveLen(1))
+			Expect(vs.Spec.Gateways[0]).To(Equal(gateway))
+		})
+	})
+
+	Describe("VirtualService Spec", func() {
+		It("should build the spec", func() {
+
+			result := VirtualServiceSpec().
+				Host(host).
+				Gateway(gateway).
+				HTTP(
+					MatchRequest().URI().Regex(matchUriRegex),
+					RouteDestination().Host(destHost).Port(destPort)).
+				Get()
+
+			Expect(result.Hosts).To(HaveLen(1))
+			Expect(result.Hosts[0]).To(Equal(host))
+
+			Expect(result.Gateways).To(HaveLen(1))
+			Expect(result.Gateways[0]).To(Equal(gateway))
+
+			Expect(result.HTTP).To(HaveLen(1))
+			Expect(result.HTTP[0].Match).To(HaveLen(1))
+			Expect(result.HTTP[0].Match[0].URI.Regex).To(Equal(matchUriRegex))
+			Expect(result.HTTP[0].Route).To(HaveLen(1))
+			Expect(result.HTTP[0].Route[0].Destination.Host).To(Equal(destHost))
+			Expect(result.HTTP[0].Route[0].Destination.Port.Number).To(Equal(destPort))
+			Expect(result.HTTP[0].Route[0].Destination.Port.Name).To(BeEmpty())
+		})
+	})
+})
