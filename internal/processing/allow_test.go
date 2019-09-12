2145a22757d8b41b152b1b760720e5bbb95cd2f6
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2019-09-12 11:52:01 +0200

    Review fix

--- internal/processing/allow_test.go
+++ internal/processing/allow_test.go
@@ -76,8 +76,8 @@ func getGate4Allow() *gatewayv2alpha1.Gate {
 					Path:    "/.*",
 					Methods: []string{"GET"},
 					Mutators: []*rulev1alpha1.Mutator{
-						{
-							&rulev1alpha1.Handler{
+						&rulev1alpha1.Mutator{
+							Handler: &rulev1alpha1.Handler{
 								Name: "noop",
 							},
 						},
