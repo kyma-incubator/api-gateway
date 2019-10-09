7a09cff37fa2019094b142e8a6730c9ef5baf4b7
Julia Iskierka <48293543+colunira@users.noreply.github.com>
2019-10-09 07:43:35 +0200

    Update error message about blacklisted service
    Co-Authored-By: Jakub Kabza <jakub.kabza@sap.com>

--- internal/validation/validate.go
+++ internal/validation/validate.go
@@ -80,7 +80,7 @@ func (v *APIRule) validateService(attributePath string, vsList v1alpha3.VirtualS
 			if svc == *api.Spec.Service.Name && namespace == api.ObjectMeta.Namespace {
 				problems = append(problems, Failure{
 					AttributePath: attributePath + ".name",
-					Message:       "Service " + svc + " in namespace " + namespace + " is blacklisted",
+					Message:       fmt.Sprintf("Service %s in namespace %s is blacklisted", svc, namespace)
 				})
 			}
 		}
